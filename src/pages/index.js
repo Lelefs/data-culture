c0d352ccf27be4cf0bad489e002e039659c9fa93
Leandro Finochio <lefinochio@hotmail.com>
2023-07-20 11:00:42 -0300

    Metatags

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,33 @@ export default function Home() {
           name="keywords"
           content="Business Intelligence, Data Science, Inside Sales, Gerenciamento de dados, Modelagem de dados, Montagem de equipe inside sales, Arquitetura de dados, Plataformas BI e Tratamento de dados"
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://data-culture.vercel.app/" />
+        <meta property="og:title" content="Data Culture" />
+        <meta
+          property="og:description"
+          content="Business intelligence (BI) e ciência de dados para coletar, analisar e interpretar dados para orientar decisões de negócios."
+        />
+        <meta
+          property="og:image"
+          content="https://imgs-readme.s3.sa-east-1.amazonaws.com/Dataculture.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://data-culture.vercel.app/"
+        />
+        <meta property="twitter:title" content="Data Culture" />
+        <meta
+          property="twitter:description"
+          content="Business intelligence (BI) e ciência de dados para coletar, analisar e interpretar dados para orientar decisões de negócios."
+        />
+        <meta
+          property="twitter:image"
+          content="https://imgs-readme.s3.sa-east-1.amazonaws.com/Dataculture.png"
+        />
       </Head>
 
       <div className={styles.container}>
